5dd91d4b3e9e9648027f2b083b4611d0585c3cdc
mesh-254 <meshack1995@gmail.com>
2022-07-21 18:19:17 +0300

    5. Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 from bcrypt import hashpw
 import bcrypt
+from typing import ByteString
 
-
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> ByteString:
     """
     Hash a password.
     """
