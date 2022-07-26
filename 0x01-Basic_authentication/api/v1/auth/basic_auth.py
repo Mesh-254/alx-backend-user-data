5c6ae322caaa7d4df626c41e8a07c4b69dec8c8c
mesh-254 <meshack1995@gmail.com>
2022-07-26 18:17:29 +0300

    Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """ 6. Basic auth"""
 import base64
-import email
-
-from click import password_option
+from models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -62,3 +60,62 @@ class BasicAuth(Auth):
         if len(user_credentials) != 2:
             return None, None
         return user_credentials[0], user_credentials[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        get the user object from the credentials
+        :param user_email:
+        :param user_pwd:
+        :return:
+        """
+        if user_email is None or user_pwd is None:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            found_users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        get the current user
+        :param request:
+        :return:
+        """
+        if request is None:
+            return None
+
+        authorization_header = self.authorization_header(request)
+        if authorization_header is None:
+            return None
+
+        base64_authorization_header = self.extract_base64_authorization_header(
+            authorization_header)
+        if base64_authorization_header is None:
+            return None
+
+        decoded_base64_authorization_header \
+            = self. \
+            decode_base64_authorization_header(base64_authorization_header)
+        if decoded_base64_authorization_header is None:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_base64_authorization_header)
+        if user_email is None or user_pwd is None:
+            return None
+
+        user = self.user_object_from_credentials(user_email, user_pwd)
+        if user is None:
+            return None
+
+        return user
