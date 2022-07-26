f4c14b0532eacd9950017c5e7536145103d34a33
mesh-254 <meshack1995@gmail.com>
2022-07-26 16:38:47 +0300

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ 6. Basic auth"""
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -21,3 +22,22 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ')[-1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 part of the Authorization header
+        :param base64_authorization_header: the Base64 part of the Authorization header
+        :return: the decoded Base64 part of the Authorization header
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        if base64_authorization_header != base64_authorization_header.encode():
+
+            try:
+                return base64.b64decode(base64_authorization_header).decode('utf-8')
+            except:
+                return None
+
+        return base64_authorization_header.decode('utf-8')
